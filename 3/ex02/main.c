50d35960122482cce2912255ff379f1bf509d3cc
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2023-03-16 15:37:47 +0100

    Finish d3 ex2

---
+++ 3/ex02/main.c
@@ -0,0 +1,42 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void uart_init() {
+	//Enable transmitter and receiver on USART0
+	UCSR0B |= (1 << TXEN0) | (1 << RXEN0);
+	//Keep defaults : async with no parity and 1 stop bit
+	//Set character size to 8 bits
+	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ00);
+	//Set baud rate to UART_BAUDRATE
+	UBRR0 = (F_CPU / 8 / UART_BAUDRATE - 1) / 2;
+}
+
+void uart_tx(char c) {
+	//Wait for USART to be ready to transmit
+	while (!(UCSR0A & (1 << UDRE0))) {}
+	//Transmit character
+	UDR0 = c;
+}
+
+char uart_rx() {
+	//Wait for USART0 to have received something
+	while (!(UCSR0A & (1 << RXC0))) {}
+	//Return received character
+	return (UDR0);
+}
+
+void uart_printstr(const char *str) {
+	for (int i = 0; str[i]; i++) {
+		uart_tx(str[i]);
+	}
+}
+
+int main() {
+	char c;
+	uart_init();
+	while (1) {
+		c = uart_rx();
+		uart_tx(c);
+	}
+}
