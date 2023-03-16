2f11b358ddac5b3692921405d97f9033b112a93b
Maxime ARNAUDY <maxime.arnaudy@gmail.com>
2023-03-16 15:45:54 +0100

    Finish d3 ex3

---
+++ 3/ex03/main.c
@@ -0,0 +1,58 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void uart_init() {
+	//Enable transmitter and receiver on USART0
+	UCSR0B |= (1 << TXEN0) | (1 << RXEN0);
+	//Keep defaults : async with no parity and 1 stop bit
+	//Set character size to 8 bits
+	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ00);
+	//Set baud rate to UART_BAUDRATE
+	UBRR0 = (F_CPU / 8 / UART_BAUDRATE - 1) / 2;
+}
+
+void uart_tx(char c) {
+	//Wait for USART to be ready to transmit
+	while (!(UCSR0A & (1 << UDRE0))) {}
+	//Transmit character
+	UDR0 = c;
+}
+
+char uart_rx() {
+	//Wait for USART0 to have received something
+	while (!(UCSR0A & (1 << RXC0))) {}
+	//Return received character
+	return (UDR0);
+}
+
+void uart_printstr(const char *str) {
+	for (int i = 0; str[i]; i++) {
+		uart_tx(str[i]);
+	}
+}
+
+char change_case(char c) {
+	if (c >= 'A' && c <= 'Z') {
+		return (c + 'a' - 'A');
+	}
+	if (c >= 'a' && c <= 'z') {
+		return (c + 'A' - 'a');
+	}
+	return (c);
+}
+
+ISR(USART_RX_vect) {
+	char c = uart_rx();
+	c = change_case(c);
+	uart_tx(c);
+}
+
+int main() {
+	uart_init();
+	//Enable interrupts globally
+	SREG |= (1 << SREG_I);
+	//Enable interrupt on USART0 receive
+	UCSR0B |= (1 << RXCIE0);
+	while (1) {}
+}
